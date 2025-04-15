5c4ef461b8fd5601177a7a0106c296b250d2dbb9
Raj Thombare <rajthombare.dev@gmail.com>
2025-04-15 20:31:57 +0530

    setup redis server for prod

--- server/src/config/redis.config.ts
+++ server/src/config/redis.config.ts
@@ -1,8 +1,16 @@
 import { Redis } from "ioredis";
 
+// const redis = new Redis({
+//     host: "localhost",
+//     port: 6379
+// })
+
 const redis = new Redis({
-    host: "localhost",
-    port: 6379
-})
+    port: Number(process.env.REDIS_PORT || 6379),
+    host: process.env.REDIS_HOST,
+    username: "default",
+    password: process.env.REDIS_PASSWORD,
+    db: 0,
+});
 
 export default redis;
