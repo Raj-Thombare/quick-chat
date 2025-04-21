35397dd42508ed66355b7960d36da978687b4886
Raj Thombare <rajthombare.dev@gmail.com>
2025-04-21 21:04:12 +0530

    fix session on client

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,16 +1,17 @@
+"use client";
+
 import FeatureSection from "@/components/base/FeatureSection";
 import Footer from "@/components/base/Footer";
 import HeroSection from "@/components/base/HeroSection";
 import Navbar from "@/components/base/Navbar";
 import UserReviews from "@/components/base/UserReviews";
-import { getServerSession } from "next-auth";
-import { authOptions, CustomSession } from "./api/auth/[...nextauth]/options";
 import HowItWorks from "@/components/base/HowItWorks";
 import Insights from "@/components/base/Insights";
 import CallToAction from "@/components/base/CallToAction";
+import { useSession } from "next-auth/react";
 
-export default async function Home() {
-  const session: CustomSession | null = await getServerSession(authOptions);
+export default function Home() {
+  const { data: session } = useSession();
 
   return (
     <div className='min-h-screen flex flex-col'>
